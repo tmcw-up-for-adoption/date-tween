8a1ab1de7aeed809c948c162fb4e88ea64a0c318
Tom MacWright <tom@macwright.org>
2013-03-14 12:02:59 -0400

    First

---
+++ README.md
@@ -0,0 +1,11 @@
+Basic date tweening
+
+## api
+
+`dateTween(startDate, endDate, duration, callback)`
+
+Tween from startDate to endDate in duration milliseconds, and call callback
+with a date object at each tween position.
+
+* `dateTweenFormatDate`: padded date formatter
+* `dateTweenFormatTime`: padded time formatter
---
+++ index.js
@@ -0,0 +1,50 @@
+function lpad(x, l, w) {
+    w = w || ' ';
+    var s = '' + x;
+    while (s.length < l) s = w + s;
+    return s;
+}
+
+function dateTweenFormatDate(d) {
+    var monthNames = [ "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December" ];
+    return lpad(monthNames[d.getMonth()], 10) + ' ' + lpad(d.getDate(), 2) + ', ' + d.getFullYear();
+}
+
+function dateTweenFormatTime(d) {
+    var h = d.getHours() + 1;
+    var ampm = 'am';
+    if (h > 12) {
+        h -= 12;
+        ampm = 'pm';
+    }
+    return lpad(h, 2) + ':' + lpad(d.getMinutes(), 2) +
+        ':' + lpad(d.getSeconds(), 2, 0) + ampm;
+}
+
+function dateTween(a, b, t, c) {
+    var as = +a,
+        bs = +b,
+        dist = bs - as,
+        start = +new Date(),
+        out = new Date(),
+        r, end = start + t;
+
+    function update() {
+        var now = +new Date();
+        if (now < end) {
+            r = ((now - start) / t);
+            c(new Date(as + (dist * r)));
+            setTimeout(update, 10);
+        } else {
+            c(new Date(bs));
+        }
+    }
+    update();
+}
+
+if (typeof module !== 'undefined') module.exports = {
+    dateTween: dateTween,
+    dateTweenFormatDate: dateTweenFormatDate,
+    dateTweenFormatTime: dateTweenFormatTime
+};
